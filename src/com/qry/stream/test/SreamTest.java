50fe9423b427c879f8b7535737167dfe2765d2e7
qurongyao <qurongyao@wison.com>
2020-06-12 13:53:10 +0800

    commit

---
+++ src/com/qry/stream/test/SreamTest.java
@@ -0,0 +1,38 @@
+package com.qry.stream.test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @ClassName SreamTest
+ * @Package com.qry.stream.test
+ * @Description
+ * @Author qurongyao
+ * @Date 2020/6/12 11:07
+ * @Version 1.0
+ */
+public class SreamTest {
+    public static void main(String[] args) {
+        List<User> users = Arrays.asList(
+                new User(1, "张三", 18),
+                new User(3, "李四", 18),
+                new User(5, "王五", 43),
+                new User(2, "赵柳", 18),
+                new User(9, "小三", 13),
+                new User(17, "曲荣耀", 12),
+                new User(10, "小七", 18),
+                new User(8, "小巴", 7),
+                new User(7, "小舞", 9),
+                new User(13, "张三", 26)
+        );
+        order(users);
+    }
+
+    public static void order(List<User> list){
+        Map<Integer, List<User>> collect = list.stream().collect(Collectors.groupingBy(item->item.getAge()));
+        List<User> users = collect.get(18);
+        users.forEach(System.out::println);
+    }
+}
